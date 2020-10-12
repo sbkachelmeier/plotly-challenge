387ac6ea55df341136f80b84523c0f530d34c5f7
Sarah Kachelmeier <sbkachelmeier@gmail.com>
2020-10-12 00:25:23 -0500

    Added initialization

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,5 +1,4 @@
-// Load in data from json 
-
+//Load in data from json 
 // Create the dropdown 
 function start(){
   d3.json("samples.json").then(function(data) {
@@ -11,11 +10,37 @@ function start(){
     })
     var firstid = samplenames[0]
     buildtable(firstid)
-    buildchart(firstid)
+    // buildchart(firstid)
 
   });  
 }
 start()
+
+// //Initialization function?
+// function InitDashboard()
+// {
+//   console.log('Calling InitDashboard()');
+//   var selector = d3.select("#selDataset");
+
+//   d3.json("samples.json").then((data) => {
+//     console.log(data);
+
+//     var sampleNames = data.names;
+
+//     //Populate the selector
+//     sampleNames.forEach((sampleId) => {
+//       selector.append("option")
+//       .text(sampleId)
+//       .property("value", sampleId);
+    
+//   });
+  // //Get the first sample Id
+  // var sampleId = sampleNames[0];
+  // console.log("Starting sample: ", sampleId);
+
+  // //Draw the graphs
+  // DrawBargraph(sampleId);
+  // DrawBubbleChart(sampleId);
 
 // Make table 
 function buildtable(sampleid){
@@ -32,48 +57,49 @@ function buildtable(sampleid){
   });
   })
 }
-//The following code is borrowed with permission from office hours with Dom on Thursday, October 1.
+
 // Make bubble chart
 function DrawBubbleChart(sampleId)
 {
-  console.log('DrawBubbleChart(${sampleId})');
+   console.log('DrawBubbleChart(${sampleId})');
 
 }
-//Make bar chart
+DrawBubbleChart();
+// //The following code is borrowed with permission from office hours with Dom on Thursday, October 1.
+// //Make bar chart
 function DrawBargraph(sampleID)
 {
-  console.log('Drawbargraph(${sampleId})');
-  d3.json("samples.json").then((data)) => {
+  console.log('DrawBargraph(${sampleId})');
+//   d3.json("samples.json").then((data)) => {
 
-    var samples = data.samples;
-    var resultArray = samples.filter(s => s.id == sampleId);
-    var result = resultArray[0];
+//     var samples = data.samples;
+//     var resultArray = samples.filter(s => s.id == sampleId);
+//     var result = resultArray[0];
 
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values;
-    var yticks = otu_ids.slice(0, 10).map(otuId) => 'OTU ${otuId}').reverse();
+//     var otu_ids = result.otu_ids;
+//     var otu_labels = result.otu_labels;
+//     var sample_values = result.sample_values;
+//     var yticks = otu_ids.slice(0, 10).map(otuId) => 'OTU ${otuId}').reverse();
 
-    var barData = {
-      x: sample_values.slice(0, 10).reverse(),
-      y: yticks,
-      type: "bar",
-      text: otu_labels.slice(0, 10).reverse(),
-      orientation: "h"
-    }
-    var barLayout = {
-      title: "Top 10 Bacteria Cultures",
-      margin: {t: 30, l: 150}
-    }
-    Plotly.newPlot("bar", barData, barLayout);
-  });
+//     var barData = {
+//       x: sample_values.slice(0, 10).reverse(),
+//       y: yticks,
+//       type: "bar",
+//       text: otu_labels.slice(0, 10).reverse(),
+//       orientation: "h"
+//     }
+//     var barLayout = {
+//       title: "Top 10 Bacteria Cultures",
+//       margin: {t: 30, l: 150}
+//     }
+//     Plotly.newPlot("bar", [barData], barLayout);
+//   });
 }
-
-// function optionChanged(newSampleId)
-// {
-//   console.log('User selected ${newSampleId}');
-//   DrawBubbleChart(newSampleId);
-//   DrawBargraph(newSampleID);
-// }
-
-//Make bubble chart
+DrawBargraph();
+// // Make changed option event handler
+function optionChanged(newSampleId)
+{
+  console.log('User selected ${newSampleId}');
+  DrawBubbleChart(newSampleId);
+  DrawBargraph(newSampleId);
+}
