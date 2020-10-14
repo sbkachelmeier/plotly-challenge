ad708d539ccf1ebf75d8807502e6a08efa079542
Sarah Kachelmeier <sbkachelmeier@gmail.com>
2020-10-13 23:31:38 -0500

    Sort of got switch function working

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -111,23 +111,8 @@ function DrawBubbleChart(sampleId)
 // // Make changed option event handler
 function optionChanged(newSampleId)
 {
-  console.log('User selected ${newSampleId}');
   DrawBubbleChart(newSampleId);
+  buildtable(newSampleId)
   // DrawBargraph(newSampleId);
 }
-
-// // Use D3 to create an event handler
-// d3.selectAll("body").on("change", updatePage);
-
-// function updatePage() {
-//   // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.selectAll("#selectOption").node();
-//   // Assign the dropdown menu item ID to a variable
-//   var dropdownMenuID = dropdownMenu.id;
-//   // Assign the dropdown menu option to a variable
-//   var selectedOption = dropdownMenu.value;
-
-//   console.log(dropdownMenuID);
-//   console.log(selectedOption);
-// }
 
